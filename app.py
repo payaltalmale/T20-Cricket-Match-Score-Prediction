bde085ff6186abd66de51639852a2f89cf3547a0
payaltalmale <payaltalmale07@gmail.com>
2025-02-11 11:13:32 +0530

    updated

--- app.py
+++ app.py
@@ -7,12 +7,10 @@ from sklearn.preprocessing import LabelEncoder
 # Initialize Flask app
 app = Flask(__name__)
 
-# Load the trained model and LabelEncoder
+# Load the trained model
 with open('best_xgb_model.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
 
-# with open('label_encoder.pkl', 'rb') as label_file:
-#     label_encoder = pickle.load(label_file)
 # Load encoders
 with open('batting_team_encoder.pkl', 'rb') as file:
     batting_team_encoder = pickle.load(file)
@@ -26,33 +24,39 @@ with open('venue_encoder.pkl', 'rb') as file:
 with open('pitch_type_encoder.pkl', 'rb') as file:
     pitch_type_encoder = pickle.load(file)
 
-with open('season_econder.pkl','rb') as file:
-    season_encoder=pickle.load(file)
+with open('season_econder.pkl', 'rb') as file:
+    season_encoder = pickle.load(file)
+
 # Home page route
 @app.route('/')
 def home():
-    # Sample data for dropdowns
     batting_teams = ['CSK', 'MI', 'PBKS', 'DC', 'KKR', 'RCB', 'SRH', 'RR', 'RPS', 'GT', 'LSG']
     bowling_teams = ['MI', 'CSK', 'DC', 'PBKS', 'RCB', 'KKR', 'RR', 'SRH', 'RPS', 'GT', 'LSG']
-    venues = ['Wankhede Stadium', 'Punjab Cricket Association IS Bindra Stadium', 'Eden Gardens', 'Rajiv Gandhi International Stadium',
-       'MA Chidambaram Stadium', 'Sawai Mansingh Stadium', 'M.Chinnaswamy Stadium', 'Maharashtra Cricket Association Stadium',
-       'Arun Jaitley Stadium', 'Holkar Cricket Stadium', 'Dr. Y.S. Rajasekhara Reddy ACA-VDCA Cricket Stadium',
-       'Saurashtra Cricket Association Stadium', 'Green Park', 'Sheikh Zayed Stadium', 'Dubai International Cricket Stadium',
-       'Sharjah Cricket Stadium', 'Narendra Modi Stadium', 'Brabourne Stadium', 'Dr DY Patil Sports Academy',
-       'Bharat Ratna Shri Atal Bihari Vajpayee Ekana Cricket Stadium', 'Barsapara Cricket Stadium', 'Himachal Pradesh Cricket Association Stadium',
-       'Maharaja Yadavindra Singh International Cricket Stadium', 'Wanderers Stadium', 'SuperSport Park, Centurion',
-       'Kingsmead Cricket Ground', 'Mangaung Oval', "St George's Park", 'De Beers Diamond Oval', 'Buffalo Park', 'Newlands Cricket Ground',
-       'Vidarbha Cricket Association (VCA) Stadium', 'Barabati Stadium', 'Jawaharlal Nehru International Stadium',
-       'JSCA International Stadium Complex', 'Shaheed Veer Narayan Singh International Cricket Stadium']
+    venues = [
+        'Wankhede Stadium', 'Punjab Cricket Association IS Bindra Stadium', 'Eden Gardens',
+        'Rajiv Gandhi International Stadium', 'MA Chidambaram Stadium', 'Sawai Mansingh Stadium',
+        'M.Chinnaswamy Stadium', 'Maharashtra Cricket Association Stadium', 'Arun Jaitley Stadium',
+        'Holkar Cricket Stadium', 'Dr. Y.S. Rajasekhara Reddy ACA-VDCA Cricket Stadium',
+        'Saurashtra Cricket Association Stadium', 'Green Park', 'Sheikh Zayed Stadium',
+        'Dubai International Cricket Stadium', 'Sharjah Cricket Stadium', 'Narendra Modi Stadium',
+        'Brabourne Stadium', 'Dr DY Patil Sports Academy', 'Bharat Ratna Shri Atal Bihari Vajpayee Ekana Cricket Stadium',
+        'Barsapara Cricket Stadium', 'Himachal Pradesh Cricket Association Stadium',
+        'Maharaja Yadavindra Singh International Cricket Stadium', 'Wanderers Stadium',
+        'SuperSport Park, Centurion', 'Kingsmead Cricket Ground', 'Mangaung Oval', "St George's Park",
+        'De Beers Diamond Oval', 'Buffalo Park', 'Newlands Cricket Ground', 'Vidarbha Cricket Association (VCA) Stadium',
+        'Barabati Stadium', 'Jawaharlal Nehru International Stadium', 'JSCA International Stadium Complex',
+        'Shaheed Veer Narayan Singh International Cricket Stadium'
+    ]
     pitch_types = ['Batting-Friendly', 'Balanced', 'Bowling-Friendly']
+
     return render_template('index.html',
                            batting_teams=batting_teams,
                            bowling_teams=bowling_teams,
                            venues=venues,
                            pitch_types=pitch_types)
+
 # Function to check if the venue is a home ground
 def is_home_ground(row, team_column):
-    # Define home venues for each team
     home_venues = {
         'MI': ['Wankhede Stadium'],
         'CSK': ['MA Chidambaram Stadium'],
@@ -69,7 +73,7 @@ def is_home_ground(row, team_column):
     venue = row['Venue']
     return 1 if team in home_venues and venue in home_venues[team] else 0
 
-# Function to categorize months into Winter, Summer, and Monsoon
+# Function to categorize months into seasons
 def categorize_season(month):
     if month in [12, 1, 2]:  # Winter months
         return "Winter"
@@ -86,34 +90,29 @@ def predict():
         batting_team = request.form['Batting Team']
         balling_team = request.form['Balling Team']
         venue = request.form['Venue']
-        match_date = request.form['Match Date']  # This may need to be processed if it's part of your features
+        match_date = request.form['Match Date']
         over_number = int(request.form['Over number'])
         score_after_over = float(request.form['score after over'])
         wickets_after_over = int(request.form['wickets after over'])
-        pitch_type = request.form['Pitch Type']  # Added Pitch Type field
-        print("printing pitch type ", pitch_type)
-        run_rate=float(score_after_over/over_number)
+        pitch_type = request.form['Pitch Type']
+        run_rate = float(score_after_over / over_number)
 
-        # Convert match_date to datetime64[ns]
+        # Convert match_date to datetime
         match_date = pd.to_datetime(match_date, format='%Y-%m-%d')
 
-        # Extract features from match_date
+        # Extract date-related features
         year = match_date.year
         month = match_date.month
         day = match_date.day
-        day_of_week = match_date.dayofweek  # Monday = 0, Sunday = 6
+        day_of_week = match_date.dayofweek
 
-        # Print the extracted values for debugging
-        print(f"Year: {year}")
-        print(f"Month: {month}")
-        print(f"Day: {day}")
-        print(f"Day of Week: {day_of_week}")
         Day_Sin = np.sin(2 * np.pi * day / 31)
-        Day_Cos = np.cos(2 * np.pi * day/ 31)
+        Day_Cos = np.cos(2 * np.pi * day / 31)
 
-        # Apply the function to create a new 'Season' column
+        # Categorize season
         Season = categorize_season(month)
-        # Prepare the input data without the Match Date
+
+        # Prepare input data
         input_data = pd.DataFrame({
             'Batting Team': [batting_team],
             'Balling Team': [balling_team],
@@ -124,44 +123,42 @@ def predict():
             'Year': [year],
             'Month': [month],
             'Day': [day],
-            'Day_Sin':[Day_Sin],
-            'Day_Cos':[Day_Cos],
-            # 'Day of Week': [day_of_week],
-            'Run Rate':[run_rate],
-            'Batting Home Ground':[0],
-            'Balling Home Ground':[0],
+            'Day_Sin': [Day_Sin],
+            'Day_Cos': [Day_Cos],
+            'Run Rate': [run_rate],
+            'Batting Home Ground': [0],
+            'Balling Home Ground': [0],
             'Pitch Type': [pitch_type],
-            'Season':[Season]
+            'Season': [Season]
         })
-        # Add columns for batting and bowling home ground (1 or 0)
+
+        # Add home ground indicators
         input_data['Batting Home Ground'] = input_data.apply(lambda row: is_home_ground(row, 'Batting Team'), axis=1)
         input_data['Balling Home Ground'] = input_data.apply(lambda row: is_home_ground(row, 'Balling Team'), axis=1)
 
-
-        # Label encode categorical features with error handling
-        print("going into encoders")
-        # Label encode categorical features
+        # Encode categorical features
         input_data['Batting Team'] = batting_team_encoder.transform(input_data['Batting Team'])
         input_data['Balling Team'] = balling_team_encoder.transform(input_data['Balling Team'])
         input_data['Venue'] = venue_encoder.transform(input_data['Venue'])
         input_data['Pitch Type'] = pitch_type_encoder.transform(input_data['Pitch Type'])
-        input_data['Season']=season_encoder.transform(input_data['Season'])
-        # Make prediction using the loaded model
-        predicted_inning_score = model.predict(input_data)
+        input_data['Season'] = season_encoder.transform(input_data['Season'])
 
-        # Render the result on the home page
+        # Predict score
+        predicted_inning_score = model.predict(input_data)
+        rounded_score = round(predicted_inning_score[0])  # Round to nearest integer
+
+        # Render the result
         return render_template('index.html',
-                               prediction_text='Predicted Inning Score: {:.2f}'.format(predicted_inning_score[0]))
+                               prediction_text=f'Predicted Inning Score: {rounded_score}')
 
     except ValueError as e:
-        # Handle label encoding or conversion errors
         return render_template('index.html',
                                error_message=f"Error: {str(e)}. Please ensure the input values are correct.")
 
     except Exception as e:
-        # Handle any other unexpected errors
         return render_template('index.html',
                                error_message=f"An unexpected error occurred: {str(e)}")
 
+# Run the Flask app
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
